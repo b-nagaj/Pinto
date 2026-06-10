f6a989fa37d9654c7e52d06a122b5ec284366e22
Chitchat2457 <bryce.nagaj@proton.me>
2026-06-10 08:44:53 -0600

    Remove test

--- src/app/services/resume/resume.ts
+++ src/app/services/resume/resume.ts
@@ -7,13 +7,28 @@
 import { Injectable } from "@angular/core";
 
 import { environment } from "../../../environments/environment"
+import { Toast } from "../toast/toast"
 
 @Injectable({
 	providedIn: "root",
 })
 export class Resume {
+	/**
+	 * @brief Constructor
+	 * @description Instantiates a Toast object
+	 * @param toastService a Toast object
+	 */
+	constructor(private toastService: Toast) {}
+
+	/**
+	 * @brief Downloads a PDF
+	 * @description Read a PDF from a Backblaze cloud storage bucket into a byte
+	 *              array by calling Basil's `/resume/download` endpoint
+	 * @return Uint8Array
+	 */
 	public async downloadResume(): Promise<Uint8Array> {
 		const downloadUrl = `${environment.basilUrl}/resume/download`;
+
 		try {
 			const response: any = await fetch(downloadUrl, {
 				method: "GET",
@@ -23,7 +38,10 @@ export class Resume {
 			});
 
 			if (!response.ok) {
-				throw new Error('Resume download failed')
+				const errorMessage: string = "Failed to download resume from Backblaze";
+
+				console.error(errorMessage);
+
 			}
 
 			const buffer: ArrayBuffer = await response.arrayBuffer();
