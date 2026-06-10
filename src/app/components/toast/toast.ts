e736146054ee23d3756f192672a1875a82078233
Chitchat2457 <bryce.nagaj@proton.me>
2026-06-10 08:15:06 -0600

    Implement toast

--- src/app/components/toast/toast.ts
+++ src/app/components/toast/toast.ts
@@ -6,10 +6,36 @@
 
 import { Component } from "@angular/core";
 
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+
 @Component({
 	selector: "app-toast",
-	imports: [],
+	imports: [ToastModule],
 	templateUrl: "./toast.html",
 	styleUrl: "./toast.scss",
 })
-export class Toast {}
+export class Toast {
+	/**
+	 * @brief Constructor
+	 * @description instantes a message service
+	 * @param messageService a MessageService object
+	 */
+	constructor(private messageService: MessageService) {}
+
+	/**
+	 * @brief Displays an error message
+	 * @description Displays an error message in the bottom right corner as an
+	 *              overlay
+	 * @param detail An error message string
+	 */
+	public showError(detail: string) {
+		        this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: detail,
+            key: 'bl',
+            life: 3000
+        });
+	}
+}
