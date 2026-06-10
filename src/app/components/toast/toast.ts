9f770bcb8d15c32b8cc8ea0a46f38faafdd92f39
Chitchat2457 <bryce.nagaj@proton.me>
2026-06-10 07:57:29 -0600

    Define a toast component

---
+++ src/app/components/toast/toast.ts
@@ -0,0 +1,15 @@
+/**
+ * =============================================================================
+ * @description Logic for the 'app-toast' component
+ * =============================================================================
+ */
+
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-toast",
+	imports: [],
+	templateUrl: "./toast.html",
+	styleUrl: "./toast.scss",
+})
+export class Toast {}
